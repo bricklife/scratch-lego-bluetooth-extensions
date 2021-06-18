7c74378d09286d6c523488062cfe3ddff56d7553
Shinichiro Oba <bricklife@gmail.com>
2021-06-18 12:55:04 +0900

    Add translations

--- scratch-vm/src/extensions/scratch3_legomario/index.js
+++ scratch-vm/src/extensions/scratch3_legomario/index.js
@@ -352,6 +352,7 @@ class Scratch3LEGOMarioBlocks {
                 'legomario.whenPants': 'ズボンが [PANTS] のとき',
                 'legomario.isPants': 'ズボンが [PANTS]',
                 'legomario.getPants': 'ズボン',
+                'legomario.setVolume': '音量を [VOLUME] %にする',
 
                 'legomario.white': '(19) 白',
                 'legomario.red': '(21) 赤',
@@ -378,6 +379,7 @@ class Scratch3LEGOMarioBlocks {
                 'legomario.whenPants': 'ズボンが [PANTS] のとき',
                 'legomario.isPants': 'ズボンが [PANTS]',
                 'legomario.getPants': 'ズボン',
+                'legomario.setVolume': 'おんりょうを [VOLUME] %にする',
 
                 'legomario.white': '(19) しろ',
                 'legomario.red': '(21) あか',
