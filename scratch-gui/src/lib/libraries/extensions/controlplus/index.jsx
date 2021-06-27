523abad2131ce24df4ff7d4e178c1aae6df698d0
Shinichiro Oba <bricklife@gmail.com>
2021-06-27 16:55:18 +0900

    Add an extension for LEGO Technic CONTROL+

---
+++ scratch-gui/src/lib/libraries/extensions/controlplus/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {FormattedMessage} from 'react-intl';
+
+import controlplusIconURL from './controlplus.png';
+import controlplusInsetIconURL from './controlplus-small.svg';
+import controlplusConnectionIconURL from './controlplus-illustration.svg';
+import controlplusConnectionSmallIconURL from './controlplus-small.svg';
+import controlplusConnectionTipIconURL from './controlplus-button-illustration.svg';
+
+const entry = {
+    name: 'LEGO Technic CONTROL+',
+    extensionId: 'controlplus',
+    extensionURL: 'https://bricklife.com/scratch-gui/xcratch/controlplus.mjs',
+    collaborator: 'bricklife',
+    iconURL: controlplusIconURL,
+    insetIconURL: controlplusInsetIconURL,
+    description: (
+        <FormattedMessage
+            defaultMessage="Build with motors and sensors."
+            id="gui.extension.controlplus.description"
+        />
+    ),
+    featured: true,
+    disabled: false,
+    bluetoothRequired: true,
+    internetConnectionRequired: true,
+    launchPeripheralConnectionFlow: true,
+    useAutoScan: true,
+    connectionIconURL: controlplusConnectionIconURL,
+    connectionSmallIconURL: controlplusConnectionSmallIconURL,
+    connectionTipIconURL: controlplusConnectionTipIconURL,
+    connectingMessage: (
+        <FormattedMessage
+            defaultMessage="Connecting"
+            id="gui.extension.controlplus.connectingMessage"
+        />
+    ),
+    helpLink: 'https://scratch.mit.edu/boost'
+};
+
+export {entry}; // loadable-extension needs this line.
+export default entry;
